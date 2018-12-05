a71b9cac0dd6d33a76dd4c718efeccce725a77ce
triest <you@example.com>
2018-12-05 16:18:32 +0300

    Add auth

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,8 @@ use Illuminate\Support\Facades\Input;
 Auth::routes();
 
 Route::get('/', 'MainController@index')->name('main');
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
