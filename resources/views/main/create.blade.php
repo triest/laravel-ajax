428f2f9d25699ccdcf06b369a55dea8945272ad9
triest <you@example.com>
2018-12-05 20:41:21 +0300

    Add dynamick filds.

--- resources/views/main/create.blade.php
+++ resources/views/main/create.blade.php
@@ -28,9 +28,46 @@
         @endif
         <br>
 
+        <table class="table table-bordered" id="dynamic_field">
+            <tr>
+                <td><input type="text" name="name[]" placeholder="Enter your Name" class="form-control name_list"/></td>
+                <td>
+                    <button type="button" name="add" id="add" class="btn btn-success">Add More</button>
+                </td>
+            </tr>
+        </table>
+
+
         <br><br>
         <button type="submit" class="btn btn-default">Создать текст</button>
     </form>
 
+    <script>
+        $(document).ready(function () {
+            var i = 1;
+            $('#add').click(function () {
+                i++;
+                $('#dynamic_field').append('<tr id="row' + i + '"><td><input type="text" name="name[]" placeholder="Enter your Name" class="form-control name_list" /></td><td><button type="button" name="remove" id="' + i + '" class="btn btn-danger btn_remove">X</button></td></tr>');
+            });
+
+            $(document).on('click', '.btn_remove', function () {
+                var button_id = $(this).attr("id");
+                $('#row' + button_id + '').remove();
+            });
+
+            $('#submit').click(function () {
+                $.ajax({
+                    url: "name.php",
+                    method: "POST",
+                    data: $('#add_name').serialize(),
+                    success: function (data) {
+                        alert(data);
+                        $('#add_name')[0].reset();
+                    }
+                });
+            });
+
+        });
+    </script>
 @endsection
 
