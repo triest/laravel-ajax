5ce0bd331420182ea899536537a8cd47487b7d83
triest <you@example.com>
2018-12-05 14:44:01 +0300

    Create models

---
+++ app/Http/Controllers/DidController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Did;
+use Illuminate\Http\Request;
+
+class DidController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Did  $did
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Did $did)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Did  $did
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Did $did)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Did  $did
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Did $did)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Did  $did
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Did $did)
+    {
+        //
+    }
+}
