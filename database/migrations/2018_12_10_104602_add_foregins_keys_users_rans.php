8fa00eddc55c86e3f708a06967828b8ed4dda478
triest <you@example.com>
2018-12-10 13:55:22 +0300

    Add users_rand tables and relationships

---
+++ database/migrations/2018_12_10_104602_add_foregins_keys_users_rans.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddForeginsKeysUsersRans extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users_rands', function (Blueprint $table) {
+            $table->foreign('users_id')->references('id')->on('users');
+            $table->foreign('rands_id')->references('id')->on('rand');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users_rands', function (Blueprint $table) {
+            $table->dropForeign('rands_id');
+            $table->dropForeign('users_id');
+        });
+    }
+}
